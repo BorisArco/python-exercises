ba529304da2da3fb8c3d75dc603f725eb4b634c6
boris.arco <boris.arco@gmail.com>
2023-05-13 18:46:44 -0400

    Transform to function

--- is-palindrome.py
+++ is-palindrome.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python3
 
 # Create a function isPalindrome
-
-word = input("Write a word: ")
+def isPalindrome(word):
 # Split the word string
-word_arr = [letter for letter in word]
+    word_arr = [letter for letter in word]
 
 # Inverted the word string
-inverted_word_arr = [word_arr[letter] for letter in range(len(word_arr)-1,-1 ,-1)]
+    inverted_word_arr = [word_arr[letter] for letter in range(len(word_arr)-1,-1 ,-1)]
 
 # Join string
-join_word_arr = ""
-for i in range(0, len(inverted_word_arr), 1):
-    join_word_arr += inverted_word_arr[i]
+    join_word_arr = ""
+    for i in range(0, len(inverted_word_arr), 1):
+        join_word_arr += inverted_word_arr[i]
 
 # Palindrome validation
-if word == join_word_arr:
-    print(f'{word} is a palindrome')
-else:
-    print(f'{word} Is not a palindrome')
+    if word == join_word_arr:
+        print(f'{word} is a palindrome')
+    else:
+        print(f'{word} Is not a palindrome')
+
+word = input("Write a word: ")
+isPalindrome(word)
